273fd811cda645304e5ef8fdca0a88d8649a5d3c
Raul Baez Amate <raul.baeza@softtekprojects.com>
2024-11-26 14:14:08 +0100

    Cambios en el on destroy

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../service/data.service';
+import { Router } from '@angular/router'; // Importa el Router
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
 
+  constructor(private dataService: DataService, private router: Router) {} 
+
+  observables() {
+    this.dataService.onShowMenu.next(false);
+
+    this.router.navigate(['/observables-suscripciones']);
+  }
+
+  ciclos(){
+    this.dataService.onShowMenu.next(false);
+
+    this.router.navigate(['/ciclos-vida']);
+  }
 }
